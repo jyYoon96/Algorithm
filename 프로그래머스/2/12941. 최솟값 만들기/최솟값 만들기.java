8e3eee2526e452d6c057a38f800ce3c73151f4b1
jyyoon <139435177+jyYoon96@users.noreply.github.com>
2025-04-16 10:20:25 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.33 ms, Memory: 53.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+        int leng = A.length;
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        for (int i=0; i<leng; i++) {
+            answer += A[i]*B[leng-1-i];
+        }
+
+        return answer;
+    }
+}
